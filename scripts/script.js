915ffa86470faf3da3156bd76fea3ae5dc3e33d8
hosen rabby <tahmid.tuhin.3@gmail.com>
2025-03-02 00:43:17 +0600

    history part updated

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const cDate = new Date();
+const crntWeekDays = days[cDate.getDay()];
+const cTrik = cDate.getDate();
+const cMonth = months[cDate.getMonth()];
+const year = cDate.getFullYear();
+
+const cDayDate = cDate.toDateString()
+const cTime = cDate.toLocaleTimeString()
+
+const currnetDate = `${cMonth} ${cTrik} ${year}`;
+
+document.getElementById("crntDay").innerText = `${crntWeekDays} ,`;
+document.getElementById("crntDate").innerText = currnetDate;
+
+const taskBtns = document.querySelectorAll(".task-btn");
+// let counter = null;
+for (const taskBtn of taskBtns) {
+    taskBtn.addEventListener("click" , function(ev){
+        const taskCount = getDataById("task-count");
+        const updateTaskCount = taskCount - 1;
+        document.getElementById("task-count").innerText = `0${updateTaskCount}`;
+        
+        const allTaskCount = getDataById("all-task-count");
+        const updateAllTaskCount = allTaskCount + 1;
+        document.getElementById("all-task-count").innerText = updateAllTaskCount;
+        
+        const taskTitle = ev.target.parentNode.parentNode.firstElementChild.nextElementSibling.innerText;
+
+        const container = document.getElementById("history");
+        const div = document.createElement("div")
+        div.innerHTML = `<div class="bg-slate-200 p-4 rounded-xl mb-3">
+                            <h1>You have Complete The Task ${taskTitle} at ${cTime}</h1>
+                        </div>`;
+        container.classList.remove("hidden")
+        container.appendChild(div);
+        
+        alert("Board Updated Successfully");
+    })
+}
---
+++ scripts/utilitys.js
@@ -0,0 +1,5 @@
+function getDataById(id) {
+    let data = document.getElementById(id).innerText;
+        data = parseInt(data);
+        return data;
+}
